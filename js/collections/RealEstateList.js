17d1db93daa7eac2700f4fa3946ae31cc40e1d29
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-17 14:56:18 +0200

    only one selected from collection

--- js/collections/RealEstateList.js
+++ js/collections/RealEstateList.js
@@ -1,28 +1,55 @@
 MapSearchApp.Collections.RealEstateList = Backbone.Collection.extend({
 
-  model: MapSearchApp.Models.RealEstate
+    model: MapSearchApp.Models.RealEstate,
 
-  // TODO: observe models isSelected()
+    // TODO: observe models isSelected()
+    initialize: function (params) {
+        this.on("change:selected", function (ob1) {
 
+//            //version 2
+            if (ob1.isSelected()) {
+                this.where({selected: true}).forEach(function (el) {
+                    if (el !== ob1)
+                        el.set('selected', false);
+                });
+            }
+
+            //version 1
+            //console.log('arguments', arguments);
+//            if (ob1.isSelected()) {
+//                this.each(function (el) {
+//                    //console.log('el', el);
+//                    if (el != ob1 && el.isSelected())
+//                        el.set('selected', false);
+//                });
+//            }
+
+        });
+    }
 });
 
 function testRealEstateList() {
-  var json = [
-    {selected: false},
-    {selected: false},
-    {selected: false}
-  ];
-  var collection = new MapSearchApp.Collections.RealEstateList(json);
+    var json = [
+        {selected: false},
+        {selected: false},
+        {selected: false}
+    ];
+    var collection = new MapSearchApp.Collections.RealEstateList(json);
 
-  var re = collection.at(1);
-  re.toggleSelected();
-  var selectedList = collection.pluck("selected");
-  console.log(selectedList, "should be: F, T, F"); // => F, T, F
+    var re = collection.at(1);
+    re.toggleSelected();
+    var selectedList = collection.pluck("selected");
+    console.log(selectedList, "should be: F, T, F"); // => F, T, F
 
-  re = collection.at(0);
-  re.toggleSelected();
-  selectedList = collection.pluck("selected");
-  console.log(selectedList, "should be: T, F, F"); // => T, F, F
+    re = collection.at(0);
+    re.toggleSelected();
+    selectedList = collection.pluck("selected");
+    console.log(selectedList, "should be: T, F, F"); // => T, F, F
+
+    re = collection.at(2);
+    re.toggleSelected();
+    selectedList = collection.pluck("selected");
+    console.log(selectedList, "should be: F, F, T"); // => F, F, T
 }
 //testRealEstateList();
 
--- js/models/RealEstate.js
+++ js/models/RealEstate.js
@@ -1,16 +1,29 @@
 MapSearchApp.Models.RealEstate = Backbone.Model.extend({
-  // TODO add methods:
-  // isSelected()
-  // toggleSelected()
+    // TODO add methods:
+    // isSelected()
+    // toggleSelected()
+
+    initialize: function (params) {
+        params = params || {};
+        this.set('selected', !!params.selected);
+    },
+
+    isSelected: function () {
+        return this.get('selected');
+    },
+
+    toggleSelected: function () {
+        this.set('selected', !this.get('selected'));
+    }
 });
 
 // TODO: comment out
 function testRealEstate() {
-  var re = new MapSearchApp.Models.RealEstate();
-  console.log(re.isSelected(), "should be false"); // false
-  re.toggleSelected();
-  console.log(re.isSelected(), "should be true"); // true
-  re.toggleSelected();
-  console.log(re.isSelected(), "should be false"); // false
+    var re = new MapSearchApp.Models.RealEstate();
+    console.log(re.isSelected(), "should be false"); // false
+    re.toggleSelected();
+    console.log(re.isSelected(), "should be true"); // true
+    re.toggleSelected();
+    console.log(re.isSelected(), "should be false"); // false
 }
 //testRealEstate();
