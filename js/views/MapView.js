f4b1288290ec246b110684f0f80ddb530201502d
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-17 16:09:50 +0200

    map rendered

--- js/views/MapView.js
+++ js/views/MapView.js
@@ -5,6 +5,16 @@ MapSearchApp.Views.MapView = Backbone.View.extend({
     render: function () {
         var html = this.template();
         this.$el.html(html);
+
+        var that = this;
+        this.collection.each(function (el1) {
+            var markerView = new MapSearchApp.Views.MarkerView({
+                model: el1
+            });
+            markerView.render();
+            that.$el.find('.map').append(markerView.$el);
+        });
+
         return this;
     }
 });
--- js/views/MarkerView.js
+++ js/views/MarkerView.js
@@ -1,5 +1,32 @@
 MapSearchApp.Views.MarkerView = Backbone.View.extend({
 
+    initialize: function (params) {
+
+        this.model.on("change:selected", function (ob1) {
+            if (this.model.isSelected()) {
+                this.$el.addClass("highlighted");
+            } else {
+                this.$el.removeClass("highlighted");
+            }
+        });
+
+    },
+
+    template: MapSearchApp.Services.templates.get("#marker-template"),
+
+    render: function () {
+        var json = this.model.toJSON();
+        var m1 = {x: json.lat * 6,
+            y: json.lng * 4};
+        var html = this.template(m1);
+        this.$el.html(html);
+        if (this.model.isSelected()) {
+            this.$el.addClass("highlighted");
+        }
+
+        return this;
+    }
+
 });
 
 // CSS class: "highlighted"
